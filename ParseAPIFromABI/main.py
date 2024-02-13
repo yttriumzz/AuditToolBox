984eab1419ba7dc6977579c8c953513da2c2dda8
Your Name <you@example.com>
2024-02-13 14:51:21 +0800

    add ParseAPIFromABI

---
+++ ParseAPIFromABI/main.py
@@ -0,0 +1,31 @@
+import json
+
+
+def main():
+    f = open("abi.json")
+    data = json.load(f)
+    f.close()
+
+    abi = json.loads(data["result"])
+
+    read_function = []
+    write_function = []
+    for interface in abi:
+        if interface["type"] == "function":
+            if interface["stateMutability"] == "view":
+                read_function.append(interface["name"])
+            else:
+                write_function.append(interface["name"])
+
+    print("===== Read Function =====")
+    for f in read_function:
+        print(f)
+
+    print()
+    print("===== Write Function =====")
+    for f in write_function:
+        print(f)
+
+
+if __name__ == "__main__":
+    main()
